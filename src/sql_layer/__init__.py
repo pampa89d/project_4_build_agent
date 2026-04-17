358955c23122393223c17ea2e4469d288e953669
pampa89d <pampa89d@gmail.com>
2026-04-17 19:36:42 +0300

    Скорректированы импорты

--- src/sql_layer/__init__.py
+++ src/sql_layer/__init__.py
@@ -1,28 +1,17 @@
-from .pipeline import (
-    CANNOT_ANSWER,
-    DEFAULT_MODEL,
-    PROMPT_INJECTION,
-    SQL_TEMPERATURE,
-    build_sql_query,
-    is_cannot_answer,
-    normalize_llm_sql_response,
-    validate_safe_sql,
-)
+from .pipeline import CANNOT_ANSWER, DEFAULT_MODEL, PROMPT_INJECTION, SQL_TEMPERATURE
+from .pipeline import main as sql_validator
 from .prompts import REVIEW_PROMPT, build_messages, build_system_prompt
 from .schema import build_prompt_values, get_schema_from_db
 
 __all__ = [
-    "build_sql_query",
     "DEFAULT_MODEL",
     "SQL_TEMPERATURE",
     "CANNOT_ANSWER",
     "PROMPT_INJECTION",
     "REVIEW_PROMPT",
+    "sql_validator",
     "build_messages",
     "build_system_prompt",
     "get_schema_from_db",
     "build_prompt_values",
-    "is_cannot_answer",
-    "normalize_llm_sql_response",
-    "validate_safe_sql",
 ]
