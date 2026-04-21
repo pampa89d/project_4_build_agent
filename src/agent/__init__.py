fff9b067f3103a0c3caacc309cfbde9767277584
pampa89d <pampa89d@gmail.com>
2026-04-21 21:47:11 +0300

    Импорты SQL flow

---
+++ src/agent/__init__.py
@@ -0,0 +1,20 @@
+from .llm_client import query_llm, raw_query_llm
+from .sql_flow import (
+    DEFAULT_TOOL_MAPPING,
+    DEFAULT_TOOLS,
+    SYSTEM_PROMPT,
+    execute_tool_calls,
+    run_sql_flow,
+    sql_layer,
+)
+
+__all__ = [
+    "query_llm",
+    "raw_query_llm",
+    "sql_layer",
+    "run_sql_flow",
+    "execute_tool_calls",
+    "DEFAULT_TOOLS",
+    "DEFAULT_TOOL_MAPPING",
+    "SYSTEM_PROMPT",
+]
